ad6aec6ac812c4829e986bf95bd24a8a21291da4
Micaela Manolio <micaela.manolio@gmail.com>
2026-06-09 21:43:45 +0200

    remove reminders

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -16,21 +16,15 @@ const auth = useAuthStore();
 const router = useRouter();
 
 const doItFor = ref("");
-const reminderEnabled = ref(false);
-const reminderTime = ref("08:00");
 
 async function loadSettings() {
 	if (auth.user) {
 		const snap = await getDoc(doc(db, "users", auth.user.uid));
 		if (snap.exists()) {
 			doItFor.value = snap.data().doItFor ?? "";
-			reminderEnabled.value = snap.data().reminderEnabled ?? false;
-			reminderTime.value = snap.data().reminderTime ?? "08:00";
 		}
 	} else {
 		doItFor.value = localStorage.getItem("doItFor") ?? "";
-		reminderEnabled.value = localStorage.getItem("reminderEnabled") === "true";
-		reminderTime.value = localStorage.getItem("reminderTime") ?? "08:00";
 	}
 }
 
@@ -38,13 +32,9 @@ async function handleSaveSettings() {
 	if (auth.user) {
 		await updateDoc(doc(db, "users", auth.user.uid), {
 			doItFor: doItFor.value,
-			reminderEnabled: reminderEnabled.value,
-			reminderTime: reminderTime.value,
 		});
 	} else {
 		localStorage.setItem("doItFor", doItFor.value);
-		localStorage.setItem("reminderEnabled", reminderEnabled.value);
-		localStorage.setItem("reminderTime", reminderTime.value);
 	}
 }
 
@@ -53,16 +43,11 @@ async function handleMigrateToGoogle() {
 
 	if (!auth.user) return;
 
-	// migra doItFor e settings
-	const data = {
+	await setDoc(doc(db, "users", auth.user.uid), {
 		doItFor: localStorage.getItem("doItFor") ?? "",
-		reminderEnabled: localStorage.getItem("reminderEnabled") === "true",
-		reminderTime: localStorage.getItem("reminderTime") ?? "08:00",
 		onboardingComplete: true,
-	};
-	await setDoc(doc(db, "users", auth.user.uid), data);
+	});
 
-	// migra motivazioni
 	const motivations = JSON.parse(localStorage.getItem("motivations") ?? "[]");
 	for (const m of motivations) {
 		await addDoc(collection(db, "users", auth.user.uid, "motivations"), {
@@ -72,12 +57,10 @@ async function handleMigrateToGoogle() {
 		});
 	}
 
-	// pulisci localStorage
 	localStorage.removeItem("doItFor");
 	localStorage.removeItem("motivations");
 	localStorage.removeItem("onboardingComplete");
-	localStorage.removeItem("reminderEnabled");
-	localStorage.removeItem("reminderTime");
+	localStorage.removeItem("isGuest");
 
 	router.push("/home");
 }
@@ -92,12 +75,6 @@ onBeforeMount(() => loadSettings());
 		<section>
 			<label>Do it for...</label>
 			<input type="text" v-model="doItFor" />
-		</section>
-
-		<section>
-			<label>Reminder giornaliero</label>
-			<input type="checkbox" v-model="reminderEnabled" />
-			<input v-if="reminderEnabled" type="time" v-model="reminderTime" />
 		</section>
 
 		<button @click="handleSaveSettings">Salva</button>
