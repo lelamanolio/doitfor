7d9693c936a0f2c1af3e8bee642da4ff48a2a431
Micaela Manolio <micaela.manolio@gmail.com>
2026-06-09 17:45:00 +0200

    ✨ base setup pwa

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Do it for",
+				short_name: "Do it for",
+				description: "A little reminder. A big why.",
+				theme_color: "#B9573A",
+				background_color: "#F5EBDD",
+				display: "standalone",
+				orientation: "portrait",
+				scope: "/",
+				start_url: "/",
+				icons: [
+					{
+						src: "/icons/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/icons/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
+});
